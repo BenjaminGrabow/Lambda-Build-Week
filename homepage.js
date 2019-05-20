fb3f057c292006bf084bbc3a05242da5c91928c6
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-05-20 14:44:33 +0200

    Added tabs with cards and styled them

--- homepage.js
+++ homepage.js
@@ -207,7 +207,7 @@ class Carousel {
     this.index = 0;
     this.img[0].style.display = "block";
     }
-    
+
     rightPicture() {
     this.img[this.index].style.display = "none";
     this.index += 1;
@@ -231,4 +231,37 @@ class Carousel {
     }
      
     let carousel = document.querySelectorAll(".carousel").forEach( el => new Carousel(el));
-     
+
+// TAB SECTION
+class TabLink {
+    constructor(tabElement){
+      this.tabElement = tabElement;
+      this.tabData = tabElement.dataset.tab;
+      this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
+      this.cards = Array.from(this.cards).map(card => new TabCard(card))
+      this.tabElement.addEventListener("click", () => this.selectTab());
+    }
+  
+    selectTab(){
+      const tabs = document.querySelectorAll(".tab");
+      tabs.forEach(tab => tab.classList.remove("active-tab"));
+      const cards = document.querySelectorAll(".card");
+      cards.forEach(card => card.style.display = "none");
+      this.tabElement.classList.add("active-tab");
+      this.cards.forEach(card => card.selectCard());
+    }
+  }
+  
+  class TabCard {
+    constructor(cardElement){
+      this.cardElement = cardElement;
+    }
+
+    selectCard(){
+      const cardsContainer = document.querySelector(".cards-container");
+      cardsContainer.style.display = "flex";
+      this.cardElement.style.display = "flex";
+    }
+  }
+  
+  let tabs = document.querySelectorAll(".tab").forEach(tab => new TabLink(tab));
